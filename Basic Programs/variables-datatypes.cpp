501199e00accecc8c9c5f0802eb155d851895b4b
Kashif <ahmed.kashif1001@gmail.com>
2023-09-26 20:59:34 +0500

    bool

--- Basic Programs/variables-datatypes.cpp
+++ Basic Programs/variables-datatypes.cpp
@@ -1,12 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-  
+int main()
+{
+
     int a = 123;
-    cout<<  a << endl;
+    cout << a << endl;
 
     char b = 'a';
     cout << b << endl;
 
+    bool b = 'a';
+    cout << b << endl;
+
+    float b = 'a';
+    cout << b << endl;
+
+    char b = 'a';
+    cout << b << endl;
 }
