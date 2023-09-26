f6e2b7372216512eeba1dc9cd2977753034e9699
Kashif <ahmed.kashif1001@gmail.com>
2023-09-26 21:27:29 +0500

    sizeof

--- Basic Programs/variables-datatypes.cpp
+++ Basic Programs/variables-datatypes.cpp
@@ -3,19 +3,37 @@ using namespace std;
 
 int main()
 {
-
-    int a = 123;
+    // Notes:
+    // variables cannot start from numbers
+    // abc
+    // ABC
+    // A1
+    // a_1
+    // 1abc
+    // abc1
+    // _abc1
+    
+    int a = 123;            // 4 bytes = 32 bits of memory
     cout << a << endl;
 
-    char c = 'a';
+    char c = 'a';           // 1 byte =8 bits of memory
     cout << c << endl;
 
-    bool bl = true;
+    bool bl = true;         // 1 bit of memory
     cout << bl << endl;
 
-    float f = 1.2;
+    float f = 1.2;          // 8 bytes = 64 bits of memory
     cout << f << endl;
 
-    double d = 1.23;
+    double d = 1.23;        // 8 bytes = 64 bits of memory
     cout << d << endl;
+
+    int size = sizeof(a);   //size of tells the size of the datatype that is being occupied
+                            //on the memory 
+
+    cout << "Size of a is: " << size << endl;
+
+
+
+
 }
