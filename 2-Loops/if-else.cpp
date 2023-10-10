ed3689f8b5e3ae7d0b547610a86f81ef4e294695
Kashif <ahmed.kashif1001@gmail.com>
2023-10-10 23:23:27 +0500

    value-change

--- 2-Loops/if-else.cpp
+++ 2-Loops/if-else.cpp
@@ -44,37 +44,37 @@ int main()
 
 //PROGRAM TO CHECK IF A IS POSITIVE, NEGATIVE OR 0
 
-   int b;
+   // int b;
 
-   cout << "Enter the value of b: " << endl;
-   cin >> b;
+   // cout << "Enter the value of b: " << endl;
+   // cin >> b;
 
-   if (b > 0){
-      cout << " A is positive" << endl;
-   } 
-   else if ( b < 0) {
-       cout << " A is negative" << endl;
-   } else{
-       cout << " A is 0" << endl;
-   }
+   // if (b > 0){
+   //    cout << " A is positive" << endl;
+   // } 
+   // else if ( b < 0) {
+   //     cout << " A is negative" << endl;
+   // } else{
+   //     cout << " A is 0" << endl;
+   // }
 
 //with two variables
-   int a,c;
+   // int a,c;
 
-   cout << "Enter the value of a: " << endl;
-   cin >> a;
+   // cout << "Enter the value of a: " << endl;
+   // cin >> a;
 
-   cout << "Enter the value of c: " << endl;
-   cin >> c;
+   // cout << "Enter the value of c: " << endl;
+   // cin >> c;
 
 
-   if (a > c){
-      cout << "A is greater" << endl;
-   } else if (c > a){
-      cout << "C is greater" << endl;
-   } else {
-      cout << "A and B are equal to zero" << endl;
-   }
+   // if (a > c){
+   //    cout << "A is greater" << endl;
+   // } else if (c > a){
+   //    cout << "C is greater" << endl;
+   // } else {
+   //    cout << "A and B are equal to zero" << endl;
+   // }
 
 //homework -> output questions
 
@@ -91,7 +91,18 @@ int main()
       cout << "NEGATIVE" << endl;
    }
 
+//q2 -> output
 
+   int a_2 = 4;
+   int b = a+1;
+
+   if((a=3)==b) {
+      cout << a_2 << endl;
+   }
+
+   else {
+      cout << a_2 << endl;
+   }
 
 
 
---
+++ 2-Loops/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+   // int a,c;
+
+   // cout << "Enter the value of a: " << endl;
+   // cin >> a;
+
+   // cout << "Enter the value of c: " << endl;
+   // cin >> c;
+
+
+   // if (a > c){
+   //    cout << "A is greater" << endl;
+   // } else if (c > a){
+   //    cout << "C is greater" << endl;
+   // } else {
+   //    cout << "A and B are equal to zero" << endl;
+   // }
