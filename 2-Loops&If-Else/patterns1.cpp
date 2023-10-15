249cdb1b69bf07dc6d0b19ad5e4eac08b383597e
Kashif <ahmed.kashif1001@gmail.com>
2023-10-15 23:25:31 +0500

    patterns

---
+++ 2-Loops&If-Else/patterns1.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+ using namespace std;
+ 
+ int main(){
+
+    int n;
+    
+    cout << "Enter a value: " << endl; 
+
+    cin >> n;
+    int i = 1;
+
+
+    while(i<=n){
+
+    }
+
+
+
+
+   
+ }
---
+++ 2-Loops&If-Else/whileloop.cpp
@@ -0,0 +1,46 @@
+ #include<iostream>
+ using namespace std;
+ 
+ int main(){
+ // int n;
+
+   // cout << "Enter your number: " << endl;
+
+   // cin >> n;
+
+   // int i=1;
+   // while (i<=n){
+     
+   //    cout << "The value of i is: " << i << endl;
+   //      i = i + 1;
+     
+   // }
+ 
+   int n;
+
+   cout << "Enter a Number: " << endl;
+
+   cin>> n;
+
+   int i = 1;
+   int sum = 0;
+
+   while (i <= n){
+      sum = sum + i;
+      i = i + 1;
+      cout << "The value of sum is: "<< sum  << endl;
+      
+   }
+
+
+
+
+
+
+
+
+
+
+
+   
+ }
