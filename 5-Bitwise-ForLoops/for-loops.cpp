9809be2e6be6dd1f479750644ba236ed68d00e30
Kashif <ahmed.kashif1001@gmail.com>
2023-10-18 03:33:24 +0500

    for-eg-2

--- 5-Bitwise-ForLoops/for-loops.cpp
+++ 5-Bitwise-ForLoops/for-loops.cpp
@@ -11,9 +11,24 @@ int main(){
 
     cout << "Printing the values from 1 to n" << endl;
 
-    for(int i = 1; i <=n; i++){
-        cout << i << endl;
+    // for(int i = 1; i <=n; i++){
+    //     cout << i << endl;
+    // }
+
+
+    for(int a = 0, b = 1, c = 2; a >= 0 && b >= 1 && c >= 2; a--, b--, c--){
+        cout << a << " " << b << " " << c << endl;
     }
+
+
+
+
+
+
+
+
+
+
 
 
 
