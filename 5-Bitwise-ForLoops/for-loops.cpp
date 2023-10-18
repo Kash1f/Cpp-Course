b6f2c3dd8737b8be14427e09b936ef32c175476c
Kashif <ahmed.kashif1001@gmail.com>
2023-10-18 23:21:53 +0500

    creating-func

--- 5-Bitwise-ForLoops/for-loops.cpp
+++ 5-Bitwise-ForLoops/for-loops.cpp
@@ -11,14 +11,14 @@ int main(){
 
     cout << "Printing the values from 1 to n" << endl;
 
-    // for(int i = 1; i <=n; i++){
-    //     cout << i << endl;
-    // }
-
-
-    for(int a = 0, b = 1, c = 2; a >= 0 && b >= 1 && c >= 2; a--, b--, c--){
-        cout << a << " " << b << " " << c << endl;
+    for(int i = 1; i <=n; i++){
+        cout << i << endl;
     }
+
+
+    // for(int a = 0, b = 1, c = 2; a >= 0 && b >= 1 && c >= 2; a--, b--, c--){
+    //     cout << a << " " << b << " " << c << endl;
+    // }
 
 
 
--- 8-Switch-Functions/functions.cpp
+++ 8-Switch-Functions/functions.cpp
@@ -1,20 +1,43 @@
 #include<iostream>
 using namespace std;
 
+int power(int a , int b){
+
+}
+
+    return ans;
+
 int main(){
 
-    //power (a,b)
+    // //power (a,b)
 
-    int a,b;
+    // int a,b;
 
-    cin >> a >> b;
+    // cin >> a >> b;
 
-    int ans = 1;
+    // int ans = 1;
 
-    for (int i = 1; i <= b; i++){
-        ans = ans * a;
-    }
+    // for (int i = 1; i <= b; i++){
+    //     ans = ans * a;
+    // }
 
-    cout << "Answer is: " << ans << endl;
+    // cout << "Answer is: " << ans << endl;
+
+
+    int a , b ;
+
+    cin >> a << >> b;
+
+    power(a,b)
+
+
+
+
+
+
+
+
+
+
 
 }
