35573f0a41ea57be96dbd2efff26217c7f3f3942
Kashif <ahmed.kashif1001@gmail.com>
2023-10-18 23:27:44 +0500

    func-done

--- 8-Switch-Functions/functions.cpp
+++ 8-Switch-Functions/functions.cpp
@@ -1,36 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int power(int a , int b){
+int power(int a, int b)
+{
 
     int ans = 1;
 
-    for (int i = 1; i <= b; i++){
+    for (int i = 1; i <= b; i++)
+    {
         ans = ans * a;
     }
 
-   return ans;
+    cout << "Answer is: " << ans << endl;
 
+    return ans;
 }
 
+int main()
+{
 
-int main(){
+    int a, b;
+    cin >> a >> b;
 
-  
-    int a , b ;
+    power(a, b);
 
-    cin >> a  >> b;
-
-    power(a,b)
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
