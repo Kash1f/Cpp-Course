6deadc46b9db7fb8ac1444aa2b8fc04466a26812
Kashif <ahmed.kashif1001@gmail.com>
2023-10-18 23:24:47 +0500

    calling-func

--- 8-Switch-Functions/functions.cpp
+++ 8-Switch-Functions/functions.cpp
@@ -3,30 +3,23 @@ using namespace std;
 
 int power(int a , int b){
 
+    int ans = 1;
+
+    for (int i = 1; i <= b; i++){
+        ans = ans * a;
+    }
+
+   return ans;
+
 }
 
-    return ans;
 
 int main(){
 
-    // //power (a,b)
-
-    // int a,b;
-
-    // cin >> a >> b;
-
-    // int ans = 1;
-
-    // for (int i = 1; i <= b; i++){
-    //     ans = ans * a;
-    // }
-
-    // cout << "Answer is: " << ans << endl;
-
-
+  
     int a , b ;
 
-    cin >> a << >> b;
+    cin >> a  >> b;
 
     power(a,b)
 
