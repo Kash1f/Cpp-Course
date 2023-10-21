ceda68e4e3547c44e0e7fd136bd070c740664fea
Kashif <ahmed.kashif1001@gmail.com>
2023-10-21 13:06:01 +0500

    &1-odd

--- 8-Switch-Functions/even-odd.cpp
+++ 8-Switch-Functions/even-odd.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 using namespace std;
 
-bool isEven(){
+
+//1- Output 1 dega incase of Even
+//2- Output 0 dega incase of Odd
+bool isEven(int num){
     
+    //kisi bhi number ka &1 true ho to wo odd number hai
+    if(num & 1){
+         return 0;      //0 return incase of odd
+    }
+   
 }
+
 
 
 int main(){
 
-    int num;
+    int a;
 
-    cin>> num;
+    cin>> a;
 
     return 0;
 }
