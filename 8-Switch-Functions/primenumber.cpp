bdfa40e1e9d4e2da191ba06b480daf77727c8af5
Kashif <ahmed.kashif1001@gmail.com>
2023-10-23 22:34:56 +0500

    return-1

--- 8-Switch-Functions/primenumber.cpp
+++ 8-Switch-Functions/primenumber.cpp
@@ -4,12 +4,28 @@ using namespace std;
 //Output 1 aya to Prime Number
 //Output 0 aya to Not a Prime Number
 
-bool isPrime(){
+bool isPrime(int n){
 
+
+    for(int i = 2; i<n; i++){
+        //agar divide hojai to not a prime number
+        if(n%i ==0 ){
+            return 0;
+        }
+    }
+        return 1;       
 }
 
 int main(){
 
-    
+        int a;
 
+        cin>>a;
+
+        if(isPrime(a)){
+        cout << "This is a Prime number";
+        }
+        else{
+            cout << "This is not a Prime number";
+        }
 }
