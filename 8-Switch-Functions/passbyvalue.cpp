5ebb8a5f2914cd0c21e2e4339beb254c2f5cb06a
Kashif <ahmed.kashif1001@gmail.com>
2023-10-24 01:00:09 +0500

    void-return

--- 8-Switch-Functions/passbyvalue.cpp
+++ 8-Switch-Functions/passbyvalue.cpp
@@ -6,6 +6,9 @@ void dummy(int n){
 
     n++;
     cout << "n is: " << n << endl;
+
+    // return; return is not needed since the function is void which means that it will not
+    // return any int/char etc data type.
 }
 
 int main(){
@@ -17,6 +20,6 @@ int main(){
     dummy(n);
 
     cout << "This is a number " << n << endl;
-  
 
+    return 0;
 }
