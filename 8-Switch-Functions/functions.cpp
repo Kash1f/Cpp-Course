01c0c5137d13ab4ed42494721facc522e0d262dd
Kashif <ahmed.kashif1001@gmail.com>
2023-10-24 00:56:44 +0500

    iterations

--- 8-Switch-Functions/functions.cpp
+++ 8-Switch-Functions/functions.cpp
@@ -4,23 +4,27 @@ using namespace std;
 int power(int a, int b)
 {
 
-    //int a,b;
-    //cin >> a >> b;
-    // int ans = 1;
+    // int a,b;
+    // cin >> a >> b;
+    //  int ans = 1;
 
     int ans = 1;
 
     for (int i = 1; i <= b; i++)
     {
         ans = ans * a;
-    }
 
-    // cout << "Answer is: " << ans << endl;
+        // .In the first iteration of the loop(i = 1), ans becomes 2 * 1 = 2
+        // .In the second iteration(i = 2), ans becomes 2 * 2 = 4
+        //  .In the third iteration(i = 3), ans becomes 2 * 4 = 8
+        //  .In the fourth and final iteration(i = 4), ans becomes 2 * 8 = 16.
+    }
 
     return ans;
 }
 
-int main(){
+int main()
+{
 
     //  int answer = power();
     // cout << "Answer is: " << answer << endl;
--- 8-Switch-Functions/passbyvalue.cpp
+++ 8-Switch-Functions/passbyvalue.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 //Function Signature(int,void etc)
-void dummy(){
-    
+void dummy(int n){
+
+    n++;
+    cout << "n is: " << n << endl;
 }
 
 int main(){
 
+    int n;
+
+    cin>> n;
+
+    dummy(n);
+
+    cout << "This is a number " << n << endl;
   
 
 }
