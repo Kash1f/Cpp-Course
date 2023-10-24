95fd61d7618dcaf1d3198a1c2b72cd733e3d65a6
Kashif <ahmed.kashif1001@gmail.com>
2023-10-24 21:49:13 +0500

    func-array

--- 9-Arrays/arraybasics.cpp
+++ 9-Arrays/arraybasics.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 using namespace std;
 
+
+void printArray(){
+    
+}
 int main(){
 
 
     //declaring an array
-
     int number[15];
 
+    //accessing an array
     cout << "Value at index number: " << number[4] << endl;
 
     cout << "Everything is fine" << endl; 
+
+    return 0;
 
 }
