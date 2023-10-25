e19ece3c412713a5a593f696da9a2abf0eb0dc3f
Kashif <ahmed.kashif1001@gmail.com>
2023-10-25 20:50:21 +0500

    array-initalize

--- 9-Arrays/arraybasics.cpp
+++ 9-Arrays/arraybasics.cpp
@@ -12,7 +12,12 @@ int main(){
     int number[15];
 
     //accessing an array
-    cout << "Value at index number: " << number[4] << endl;
+    cout << "Value at index number: " << number[14] << endl;
+
+    //intializing an array
+    int second[3] = {5,7,11}
+
+    cout << "The value of the 2nd index is: " << second << endl;
 
     cout << "Everything is fine" << endl; 
 
