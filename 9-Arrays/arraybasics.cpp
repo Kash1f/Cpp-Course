6154f81b10bc203de41991cdca4bbae13e9cbe07
Kashif <ahmed.kashif1001@gmail.com>
2023-10-25 21:17:01 +0500

    fourth

--- 9-Arrays/arraybasics.cpp
+++ 9-Arrays/arraybasics.cpp
@@ -1,37 +1,61 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-void printArray(){
-    
+void printArray()
+{
 }
-int main(){
+int main()
+{
 
+    // declaring an array
+    //  int number[15];
 
-    //declaring an array
-    // int number[15];
+    // accessing an array
+    //  cout << "Value at index number: " << number[14] << endl;
 
-    //accessing an array
-    // cout << "Value at index number: " << number[14] << endl;
+    // intializing an array
 
-    //intializing an array
-
-    int second[3] = {5,7,11};
+    int second[3] = {5, 7, 11};
 
     cout << "The value of the 2nd index is: " << second[2] << endl;
 
-    //printing an array
+    // printing an array
 
-    int third[15] = {2,7};  //initializing an array
+    int third[15] = {2, 7}; // initializing an array
 
-    int n = 15; //declaring a var to use it with loop to print all array values
+    int n = 15; // declaring a var to use it with loop to print all array values
 
-    for(int i = 0; i < n; i++){  //i=0 bcz array starts from 0 till n-1(0-15) here
-        cout << third[i] << " " ;
+    for (int i = 0; i < n; i++)
+    { // i=0 bcz array starts from 0 till n-1(0-15) here
+        cout << third[i] << " ";
     }
+
+    // initializng all locations with 0
+
+    int fourth[10] = {0};
+
+    int n = 10;
+
+    for (int i = 0; i < n; i++)
+    { // i=0 bcz array starts from 0 till n-1(0-15) here
+        cout << fourth[i] << " ";
+    }
+
+    //initializng all locations with 1
+
+    int fifth[10] = {1};
+
+    int n = 10;
+
+    for (int i = 0; i < n; i++) {         // i=0 bcz array starts from 0 till n-1(0-15) here
+        cout << fifth[i] << " ";
+    }
+
+
 
 
 
     return 0;
 
+    
 }
