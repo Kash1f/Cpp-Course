f23e15fbaf4d6ac11b4bf6ddcda5bb1755016e58
Kashif <ahmed.kashif1001@gmail.com>
2023-10-28 23:02:01 +0500

    num-100

--- 9-Arrays/max_min.cpp
+++ 9-Arrays/max_min.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 int getMax(){
-    
+
 }
 
 
 
 int main(){
 
+    int size;
+    cin>>size;
+
+    int num [100];
+
+    for(int i = 0; i<size; i++){
+        cin>>num[i];
+    }
 }
