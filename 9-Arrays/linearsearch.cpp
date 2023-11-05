9d55317f4e93b247aac154ad12d3e5fbf8f1ed8e
Kashif <ahmed.kashif1001@gmail.com>
2023-11-05 23:31:49 +0500

    reverseArray

--- 9-Arrays/linearsearch.cpp
+++ 9-Arrays/linearsearch.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int search(){
+bool search(int arr[], int size, int key){
+
+    for(int i = 0; i<size; i++){
+        if(arr[i] == key) {
+            return 1;
+        }
+    }   
+
+    return 0;
 
 }
 
@@ -9,7 +17,22 @@ int search(){
 
 int main(){
 
-    int arr[10] = {5, 7, -3, 10,22, -2, 0, 5, 22, 1}
+    int arr[10] = {5, 7, -3, 10,22, -2, 0, 5, 22, 1};
  
-    //whether 1 is present in it or not?
+    cout << "Enter the element to search for" << endl;
+
+    int key;
+    cin>>key;
+
+    bool found = search(arr,10,key);
+
+    if (found) {
+        cout << "Key is present" << endl;
+    }
+
+    cout << "Key is absent" << endl;
+
+    return 0;
+
+
 }
