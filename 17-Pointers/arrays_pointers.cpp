bf5b8dfb10e7abb37b7a45d90c92ecbb26e5d60e
Kashif <ahmed.kashif1001@gmail.com>
2023-11-18 23:46:57 +0500

    mathMarks

--- 17-Pointers/arrays_pointers.cpp
+++ 17-Pointers/arrays_pointers.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int main(){
+int main()
+{
 
     int marks[5] = {23, 32, 44, 54, 98};
 
@@ -21,14 +21,19 @@ int main(){
     mathMarks[2] = 98;
     mathMarks[3] = 99;
 
-    cout <<"The math marks are: " << endl;
+    cout << "The math marks are: " << endl;
+
+    cout << mathMarks[0] << endl;
+    cout << mathMarks[1] << endl;
+    cout << mathMarks[2] << endl;
+    cout << mathMarks[3] << endl;
 
     for (int i = 0; i < 4; i++)
     {
         cout << "The value of marks: " << i << endl;
     }
-    
 
+    int *p = marks;
 
-
+    cout << "The value of marks is: " << *p << endl;
 }
