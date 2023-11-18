5a8cb2f05070264179a1d4f25048f3931213c41e
Kashif <ahmed.kashif1001@gmail.com>
2023-11-18 23:43:56 +0500

    forloop-array

--- 17-Pointers/arrays_pointers.cpp
+++ 17-Pointers/arrays_pointers.cpp
@@ -13,7 +13,22 @@ int main(){
     cout << marks[2] << endl;
     cout << marks[3] << endl;
     cout << marks[4] << endl;
+
+    int mathMarks[4];
+
+    mathMarks[0] = 89;
+    mathMarks[1] = 88;
+    mathMarks[2] = 98;
+    mathMarks[3] = 99;
+
+    cout <<"The math marks are: " << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "The value of marks: " << i << endl;
+    }
     
+
 
 
 }
